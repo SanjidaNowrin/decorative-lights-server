1940fd16f539bfd46572ed1cbdfcc3d75fbbcb59
SanjidaNowrin <nowrin.cse18@gmail.com>
2023-02-23 11:40:44 +0600

    cyclic

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uj11r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.CYCLIC_USER}:${process.env.CYCLIC_PASS}@cluster0.uj11r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
 
 //console.log(uri);
