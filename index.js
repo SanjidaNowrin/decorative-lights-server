e70b4d3c0f9edce4f3f05173473709da73e3d052
SanjidaNowrin <nowrin.cse18@gmail.com>
2023-02-24 10:05:41 +0600

    vercel

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ require("dotenv").config();
 
 const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.MONGO_CONNECTION_STRING;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uj11r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+
 //console.log(uri);
 
 const client = new MongoClient(uri, {
@@ -223,3 +225,5 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("Running Decorative Lights Site", port);
 });
+
+module.exports = app;
